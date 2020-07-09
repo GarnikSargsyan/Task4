85fb0d50da014f583062097778bca9edde7d1cb0
Garnik <garnik.sargsyan@gmail.com>
2020-07-09 22:39:49 +0400

    First JS task is done

---
+++ Task4.js
@@ -0,0 +1,28 @@
+let a=10
+let b=5
+
+
+//Sum
+let sum=a+b;
+console.log(sum);
+alert(sum);
+
+//difference (between a and b)
+let difference=a-b;
+console.log(difference)
+alert(difference)
+
+//product
+let product=a*b;
+console.log(product)
+alert(product)
+
+//quotient (between a and b)
+let quotient=a/b
+console.log(quotient)
+alert(quotient)
+
+//the remainder (between a and b)
+let remainder= a % b
+console.log(remainder)
+alert(remainder)
